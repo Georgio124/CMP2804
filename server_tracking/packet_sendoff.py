51a83eb6b21fc30c12d11a84cf9ae68c2d5fc99d
WoodsyTheNerd <26545422@students.lincoln.ac.uk>
2024-05-07 17:42:27 +0100

    Made a sample FastAPI packet sendoff for sam

--- server_tracking/packet_sendoff.py
+++ server_tracking/packet_sendoff.py
@@ -1,4 +1,28 @@
 import requests
+import os
+from dotenv import load_dotenv
+import time
 
-env_path = os.path.join(os.path.dirname(__file__), '..', 'SQL.env')
+env_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '.env')
 load_dotenv(dotenv_path=env_path)
+
+
+# URL of the FastAPI endpoint - pulls from the .env file in the root directory
+fastapi_ip = os.getenv('FastAPI_IP')
+fastapi_port = os.getenv('FastAPI_Port')
+
+url = f"http://{fastapi_ip}:{fastapi_port}/api/system/rules"
+
+# Sample data (you would need to adjust this to variable/a dynamic form of data)
+data = {
+    "ip": "10.11.12.13",
+    "allow_deny": "Allow",
+    "protocol": "ALL",
+    "weight": 5,
+    "id": 32 
+}
+
+# Sends the request to fastAPI and prints the response
+response = requests.post(url, json=data)
+print(response.status_code)
+print(response.json())
